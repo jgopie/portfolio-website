50156ebb008cc6f0062b724a7bbfd4d82e957c08
Jordan Ricardo Gopie <jordangopie@live.com>
2023-05-27 16:04:33 -0400

    Fix images

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,8 +5,8 @@ export const Footer = () => {
     return (
         <div className='footer'>
             <div className='svgCard'>
-                <p><a href="https://github.com/jgopie"><img src="github.svg"/></a></p>
-                <p><a href="https://www.linkedin.com/in/jordan-gopie-429904239/"><img src="linkedin.svg"/></a></p>
+                <p><a href="https://github.com/jgopie"><img src="/github.svg"/></a></p>
+                <p><a href="https://www.linkedin.com/in/jordan-gopie-429904239/"><img src="/linkedin.svg"/></a></p>
             </div>
             <p>© Jordan Gopie {date.getFullYear()}</p>
         </div>
