e9946e84aca154cd5427fb1bb499856b784b45a3
Jordan Ricardo Gopie <jordangopie@live.com>
2023-05-27 15:55:48 -0400

    Add social media icons and content margin

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,14 @@
+import './footer.styles.css';
+
 export const Footer = () => {
     const date = new Date();
     return (
-        <>
-            <p>© Jordan Gopie {date.getFullYear()} </p>
-        </>
+        <div className='footer'>
+            <div className='svgCard'>
+                <p><a href="https://github.com/jgopie"><img src="github.svg"/></a></p>
+                <p><a href="https://www.linkedin.com/in/jordan-gopie-429904239/"><img src="linkedin.svg"/></a></p>
+            </div>
+            <p>© Jordan Gopie {date.getFullYear()}</p>
+        </div>
     );
 }
