d35f8e71744e17d2472235d67624e8e5fae6ab96
Jordan Ricardo Gopie <jordangopie@live.com>
2026-01-27 15:50:30 -0400

    added adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
+  adapter: netlify(),
   integrations: [react()],
   server: {host: "127.0.0.1"},
   vite: {
